9d58341dd13c8c023861b8294bce1cb24821a07e
Jonathan Colon <jonatron2.0@gmail.com>
2018-05-12 19:37:43 -0400

    simplified uploadMessage method

--- app/src/main/kotlin/nyc/jsjrobotics/palmrgb/firebase/datamanagers/MessageDataManager.kt
+++ app/src/main/kotlin/nyc/jsjrobotics/palmrgb/firebase/datamanagers/MessageDataManager.kt
@@ -1,7 +1,6 @@
 package nyc.jsjrobotics.palmrgb.firebase.datamanagers
 
 import nyc.jsjrobotics.palmrgb.database.MutableMessage
-import nyc.jsjrobotics.palmrgb.firebase.Firebase.MESSAGE_MODEL
 import nyc.jsjrobotics.palmrgb.firebase.FirebaseDatabaseReference
 import java.util.*
 import javax.inject.Inject
@@ -10,18 +9,12 @@ class MessageDataManager
 @Inject constructor() {
 
     fun uploadMessage(message: MutableMessage) {
-        // creates a unique key identifier
-        val uniqueMessageIdentifier = HashMap<String, Any>()
-        // appends chatroom with unique key
-        FirebaseDatabaseReference.messageReference.updateChildren(uniqueMessageIdentifier)
-        // create a Firebase push key and set it as the message ID
-        message.messageId = FirebaseDatabaseReference.messageReference.push().key
-        // references the object using the message ID in the database
-        val messageRoot = FirebaseDatabaseReference.messageReference.child(message.messageId)
-        // assigns values to the children of the new message object
-        val messageModelMap = HashMap<String, Any>()
-        messageModelMap[MESSAGE_MODEL] = message
-        // confirm changes
-        messageRoot.updateChildren(messageModelMap)
+        message.messageId = generatePushKey()
+        FirebaseDatabaseReference
+                .messageReference
+                .child(message.messageId)
+                .setValue(message)
     }
+
+    private fun generatePushKey(): String = FirebaseDatabaseReference.messageReference.push().key
 }
